352f3e34fce4b142f8fdd528f86e23a1c0a90644
Mihail Selezniov <mihailselezniov@yandex.ru>
2015-07-09 18:31:24 +0300

    second commit

--- with_recursive.sql
+++ with_recursive.sql
@@ -3,27 +3,32 @@ SELECT
 	tmp.price
 FROM
 (
-	WITH RECURSIVE
-		cat_tree (id, parent_id, price) AS (
-			SELECT t.id, t.parent_id, t.price
-			FROM table_1 t
-			WHERE t.price IS NULL
+	SELECT
+		tmp.id,
+		tmp.price
+	FROM
+	(
+		WITH RECURSIVE
+			cat_tree (id, parent_id, price) AS (
+				SELECT t.id, t.parent_id, t.price
+				FROM table_1 t
+				WHERE t.price IS NULL
 
-			UNION ALL
+				UNION ALL
 
-			SELECT ct.id, t.parent_id, t.price
-			FROM
-				table_1 t
-				JOIN cat_tree ct ON ct.parent_id = t.id AND ct.price IS NULL
-		)
-	SELECT *
-	FROM cat_tree
-	WHERE cat_tree.price IS NOT NULL
-) tmp
+				SELECT ct.id, t.parent_id, t.price
+				FROM
+					table_1 t
+					JOIN cat_tree ct ON ct.parent_id = t.id AND ct.price IS NULL
+			)
+		SELECT *
+		FROM cat_tree
+		WHERE cat_tree.price IS NOT NULL
+	) tmp
 
-UNION ALL
+	UNION ALL
 
-SELECT t.id, t.price
-FROM table_1 t
-WHERE t.price IS NOT NULL
-;
+	SELECT t.id, t.price
+	FROM table_1 t
+	WHERE t.price IS NOT NULL) tmp
+ORDER BY tmp.id;
